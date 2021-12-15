24ea0b7506aa7f209e28d2bc97de8848a838333d
DevBadi <ayoubbouchedoub@gmail.com>
2021-12-15 16:21:34 +0100

    User Register

--- base/views.py
+++ base/views.py
@@ -7,6 +7,7 @@ from .forms import RoomForm
 from django.db.models import Q
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -18,6 +19,7 @@ from django.contrib.auth.decorators import login_required
 # ]
 
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     if request.method == 'POST':
@@ -35,13 +37,31 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Username OR password does not exit')
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Error occurred during register')
+
+    context = {'form': form}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
